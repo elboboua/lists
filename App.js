3dc2e4e9f4accfb71cf4688cfd0fec08d548d30e
elboboua <elboboua@oregonstate.edu>
2020-11-22 21:02:24 -0600

    second project for react native

--- App.js
+++ App.js
@@ -1,13 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity, SafeAreaView, Alert } from 'react-native';
 
 export default function App() {
+
+  const [people, setPeople] = useState([
+    {
+      name: 'Ahmad',
+      age: 27,
+      profession: 'Cook',
+      id: 1,
+    },
+    {
+      name: 'Ahmad',
+      age: 27,
+      profession: 'Cook',
+      id: 2,
+    },
+    {
+      name: 'Ahmad',
+      age: 27,
+      profession: 'Cook',
+      id: 3,
+    },
+    {
+      name: 'Ahmad',
+      age: 27,
+      profession: 'Cook',
+      id: 4,
+    },
+    {
+      name: 'Ahmad',
+      age: 27,
+      profession: 'Cook',
+      id: 5,
+    },
+    {
+      name: 'Ahmad',
+      age: 27,
+      profession: 'Cook',
+      id: 6,
+    },
+    {
+      name: 'Ahmad',
+      age: 27,
+      profession: 'Cook',
+      id: 7,
+    },
+  ])
+
+  const pressHandler = (name) => {
+    Alert.alert(
+      `This is ${name}'s button`,
+      "Stop touching me"
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem= { ({item}) => (
+          <TouchableOpacity onPress={() => pressHandler(item.name)}>
+            <Text style={styles.listItem}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -18,4 +79,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  listItem: {
+    fontSize: 40,
+    backgroundColor: 'orange',
+    padding: 10,
+    margin: 10,
+  }
 });
